91c97e38257e0c471990f13df18d621fe65991d6
Gilbert <akhamgilbert@gmail.com>
2024-05-25 22:13:01 +0530

    add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/about",
+        destination: "/about",
+      },
+      {
+        source: "/features",
+        destination: "/features",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
